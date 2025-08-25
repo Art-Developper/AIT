637dd809a0b8cf94ada9189811c148dd314178f2
Artyom Santoyan <artyomsantoyan.ait@gmail.com>
2025-08-25 12:19:07 +0400

    AitTeam

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,16 +1,6 @@
 
 
-import AitTeam from "../assets/AitTeam.PNG"
-import AitTeam2 from "../assets/AitTeam2.PNG"
-import AitTeam3 from "../assets/AitTeam3.PNG"
-import AitTeam4 from "../assets/AitTeam4.PNG"
-import AitTeam5 from "../assets/AitTeam5.PNG"
-import AitTeam6 from "../assets/AitTeam6.PNG"
-import AitTeam7 from "../assets/AitTeam7.PNG"
-import AitTeam8 from "../assets/AitTeam8.PNG"
-import AitTeam9 from "../assets/AitTeam9.PNG"
-import AitTeam10 from "../assets/AitTeam10.PNG"
-import BestTeacher from "../assets/BestTeacher.PNG"
+
 import OurLogo from "../assets/OurLogo.PNG"
 
 
@@ -19,99 +9,7 @@ const AboutUS = () =>{
     return (
     <>
     
-    
-           <section className="AitTeam">
-            <h2 className="team-section-title">AIT թիմը</h2>
-            <div className="Ait-Team-Personal">
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Երվանդ Թադևոսյան</h3>
-                <p className="team-position">Համահիմնադիր</p>
-                </div>
 
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam2} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Ռաֆայել Ջամալյան</h3>
-                <p className="team-position">Համահիմնադիր</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={BestTeacher} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Պաշ Կարապետյան</h3>
-                <p className="team-position">Դպրոցի տնօրեն և Web ծրագրավորման դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam3} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Գափ Ավոյան</h3>
-                <p className="team-position">AIT Kids և Web ծրագրավորման դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam4} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Վան Սայադյան</h3>
-                <p className="team-position">UI/UX դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam5} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Մարինե Ավոյան</h3>
-                <p className="team-position">Գրաֆիկ դիզայն դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam6} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Ժաննա Մելքոնյան</h3>
-                <p className="team-position">AIT Kids դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam7} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Գևորգ Մարգարյան</h3>
-                <p className="team-position">Համակարգչային գրագիտության դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam8} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Թագուհի Սիմոնյան</h3>
-                <p className="team-position">SMM դասընթացավար</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam9} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Էդ Ոսկանյան</h3>
-                <p className="team-position">Մարքեթինգ մենեջեր</p>
-                </div>
-
-                <div className="team-card">
-                <div className="team-image-container">
-                    <img src={AitTeam10} alt="Երվանդ Թաղոյան" className="team-image" />
-                </div>
-                <h3 className="team-name">Գայանե Գրիգորյան</h3>
-                <p className="team-position">SMM, Content creator</p>
-                </div>
-            </div>
-            </section>
                <section className="logo-section">
                 <div className="logo-content">
                   <h1 className="logo-title">Մեր լոգոտիպը</h1>
