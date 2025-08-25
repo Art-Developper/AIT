5a61625e2f425d898c991cc00e44e06317782a72
Artyom Santoyan <artyomsantoyan.ait@gmail.com>
2025-08-25 12:41:43 +0400

    ContactPage Completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/courses" element={<CoursesPage />} />
-        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,8 +1,12 @@
+import Header from "../components/Header";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 function ContactPage(){
     return(<>
+    <Header></Header>
     <Contact/>
+    <Footer></Footer>
     </>)
     
 }
