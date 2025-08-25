958b0b1eae518ee8f42885d110b612758d2a4b06
Artyom Santoyan <artyomsantoyan.ait@gmail.com>
2025-08-25 13:50:01 +0400

    Update

--- src/pages/Fantasy.jsx
+++ src/pages/Fantasy.jsx
@@ -1,56 +1,18 @@
 import React from "react";
+import Header from "../components/Header";
 import "./Fantasy.css";
-import Fantasy from "../assets/FantasySpace.PNG";
+import FantasyComponent from "../components/FantasySpaceComponent";
+import Footer from "../components/Footer";
 import MapIMG from "../assets/Map.PNG";
 import Team from "../assets/Team.PNG";
 
 const FantasySpace = () => {
   return (
     <>
-      <section className="hero-section">
-        <div className="hero-content">
-          <h1 className="hero-title">Fantasy Space</h1>
-          <p className="hero-subtitle">
-            AIT-ին կից գործում է Fantasy Space ծրագրավորման ընկերությունը՝
-            նորարար պրոֆեսիոնալների թիմ, ովքեր կյանքի են կոչում ամենաանհնարին
-            ֆանտազիաները։ Թվային տիեզերքում Fantasy Space-ը հանդիսանում է այն
-            առաջնորդող աստղը, որն ուղղորդում է ընկերություններին դեպի հաջողություն:
-          </p>
-          <div className="hero-actions">
-            <button className="btn-primary">Իմանալ ավելին</button>
-          </div>
-        </div>
+      <Header/>
+      <FantasyComponent></FantasyComponent>
 
-        <div>
-          <img src={Fantasy} alt="Fantasy Space" className="hero-image" />
-        </div>
-      </section>
-
-      <section className="features-section">
-        <h2 className="features-title">
-          <b>Fantasy Space</b>-ի մասին բազմաթիվ{" "}
-          <span className="gradient-text">տվյալներ</span>
-        </h2>
-
-        <div className="features-cards">
-          <div className="feature-card">
-            <h3>20</h3>
-            <p>Հոգանոց պրոֆեսիոնալների թիմ</p>
-          </div>
-          <div className="feature-card">
-            <h3>10,000+</h3>
-            <p>ներբեռնում ունեցող հավելվածների ստեղծում</p>
-          </div>
-          <div className="feature-card">
-            <h3>40+</h3>
-            <p>Հաջողված պրոյեկտներ</p>
-          </div>
-          <div className="feature-card">
-            <h3>12+</h3>
-            <p>Ոլորտ, որոնց հետ համագործակցում ենք</p>
-          </div>
-        </div>
-      </section>
+      <Footer/>
 
       <section className="clients-map-section">
         <h2 className="clients-map-title">Հաճախորդների աշխարհագրությունը</h2>
